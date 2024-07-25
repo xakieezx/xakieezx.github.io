8b77e0c6d99675e36106c4f1c1726ef9bfa7e831
Fikri <a.fikrifadzil@gmail.com>
2024-07-25 10:08:23 +0800

    favicon fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
 	title: 'akieez',
 	description: 'Nextjs app',
+	icons: {
+		icon: '/site/favicon.ico',
+	},
 };
 
 export default function RootLayout({
--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -129,11 +129,11 @@ export const Icons = {
 			viewBox="0 0 54.971 54.971"
 			// xml:space="preserve"
 		>
-			<g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+			<g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
 			<g
 				id="SVGRepo_tracerCarrier"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			></g>
 			<g id="SVGRepo_iconCarrier">
 				<g>
